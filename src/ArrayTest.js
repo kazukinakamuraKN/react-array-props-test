03193a6b9359505bdeebe94bc5bce19303b81cd4
Kazuki Nakamura <kajirikajiri@gmail.com>
2018-12-23 13:22:45 +0900

    save

--- src/ArrayTest.js
+++ src/ArrayTest.js
@@ -2,11 +2,31 @@ import React, { Component } from 'react';
 
 import './ArrayTest.css';
 
-class ArrayTest extends Component {
+class Borard extends Component {
   render() {
     return (
       <div className="ArrayTest">
-        HelloWorld
+        <p>
+          {this.props.value}
+        </p>
+      </div>
+    )
+  }
+}
+
+class ArrayTest extends Component {
+  renderBoard(i) {
+    return (
+      <div>
+        <Borard value={i} />
+        <Borard value={i} />
+      </div>
+    )
+  }
+  render() {
+    return (
+      <div>
+        {this.renderBoard("Hello")}
       </div>
     );
   }
