118329c724ff5fc007e27502647b3457fe2107de
Kazuki Nakamura <kajirikajiri@gmail.com>
2018-12-23 12:02:58 +0900

    hello,world

---
+++ src/ArrayTest.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+import './ArrayTest.css';
+
+class ArrayTest extends Component {
+  render() {
+    return (
+      <div className="ArrayTest">
+        HelloWorld
+      </div>
+    );
+  }
+}
+
+export default ArrayTest;
